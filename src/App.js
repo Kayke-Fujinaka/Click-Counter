909aa87ebe4d87c422204edf0d084a944d345bba
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-05-13 10:00:02 -0300

    [Finish] Completed project operation

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import './styles.css'
 
 export default function Contador() {
   const [count, setCount] = useState(0)
+  const [vezes, setVezes] = useState("")
+
+  function aumentarClick() {
+    setCount(count + 1)
+    if (count === 0) {
+      setVezes("vez")
+    } else if (count > 0) {
+      setVezes("vezes")
+    }
+    return
+  }
 
   function diminuirClick() {
     if (count > 0) {
       setCount(count - 1)
     }
+    if (count === 1) {
+      setVezes("")
+    } else if (count === 2) {
+      setVezes("vez")
+    }
   }
+
+
 
   return (
     <>
       <div className='container'>
-        <h1> Você clicou: {count}</h1>
+        <h1> Você clicou: {count} {vezes}</h1>
         <div>
           <button onClick={diminuirClick}>-</button>
-          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={aumentarClick}>+</button>
         </div>
       </div>
     </>
